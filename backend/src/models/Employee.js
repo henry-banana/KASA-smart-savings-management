f369f690769cdb4bc4113752e3e64cf668547d05
Le Tan Hiep <tanhiep24135@gmail.com>
2025-11-11 19:11:42 +0700

    [FE]Stop tracking backend folder locally

--- backend/src/models/Employee.js
+++
@@ -1,9 +0,0 @@
-import { BaseModel } from "./BaseModel.js";
-
-class EmployeeModel extends BaseModel {
-  constructor() {
-    super("employee", "employeeid");
-  }
-}
-
-export const Employee = new EmployeeModel();
