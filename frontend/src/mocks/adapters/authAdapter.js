2d022e9def5d8145cab6a9e3a6df8a367e1e3022
TonKnight <tanhiep24135@gmail.com>
2025-11-22 01:08:16 +0700

    [chore][FE] add status to mock

--- frontend/src/mocks/adapters/authAdapter.js
+++ frontend/src/mocks/adapters/authAdapter.js
@@ -14,11 +14,16 @@ export const mockAuthAdapter = {
       throw new Error('Tên đăng nhập hoặc mật khẩu không đúng');
     }
     
+    if (user.status !== 'active') {
+      throw new Error('Tài khoản đã bị vô hiệu hoá');
+    }
+
     return {
       userId: user.userid,
       username: user.userid, // username: user.username,
       roleName: user.role,
       fullName: user.fullName,
+      status: user.status,
       token: `mock_token_${user.userid}_${Date.now()}`
     };
   },
