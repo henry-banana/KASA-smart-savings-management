2d6021919ef516d7af926bc94944697d4478e72e
Phuc Hau <phuchau.fit@gmail.com>
2025-11-28 12:15:10 +0700

    feat: api daily report

--- backend/src/routers/report.router.js
+++ backend/src/routers/report.router.js
@@ -2,9 +2,6 @@ import express from "express";
 import {
   getDailyReport,
   getMonthlyReport,
-  getInterestReport,
-  getTransactionsReport,
-  getSavingBookSummary
 } from "../controllers/Report/report.controller.js";
 
 const router = express.Router();
@@ -12,8 +9,5 @@ const router = express.Router();
 
 router.get("/daily", getDailyReport);
 router.get("/monthly", getMonthlyReport);
-router.get("/interest", getInterestReport);
-router.get("/transactions", getTransactionsReport);
-router.get("/savingbook-summary", getSavingBookSummary);
 
 export default router;
