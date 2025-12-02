c54917fbf5cf6016fc9623d1e51a1ee9206746aa
Phuc Hau <phuchau.fit@gmail.com>
2025-12-02 16:34:34 +0700

    feat: get all branch name

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors"; 
+import cors from "cors";
 import { setupSwagger } from "./src/config/swagger.js";
 import authRoutes from "./src/routers/userAccount.router.js";
-import savingBookRoutes from './src/routers/savingBook.router.js';
-import transactionRoutes from './src/routers/transaction.router.js';
-import customerRoutes from './src/routers/customer.router.js';
-import typeSavingRoutes from './src/routers/typeSaving.router.js';
-import reportRoutes from './src/routers/report.router.js';
-import userRoutes from './src/routers/userAccount.router.js';
-
+import savingBookRoutes from "./src/routers/savingBook.router.js";
+import transactionRoutes from "./src/routers/transaction.router.js";
+import customerRoutes from "./src/routers/customer.router.js";
+import typeSavingRoutes from "./src/routers/typeSaving.router.js";
+import reportRoutes from "./src/routers/report.router.js";
+import userRoutes from "./src/routers/userAccount.router.js";
+import branchRoutes from "./src/routers/branch.router.js";
 
 dotenv.config();
 
@@ -17,11 +17,13 @@ const app = express();
 setupSwagger(app);
 
 // CORS: cho phép frontend từ domain/port khác gọi API
-app.use(cors({
-  origin: "http://localhost:5173", 
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true, // nếu cần gửi cookie
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // nếu cần gửi cookie
+  })
+);
 
 app.use(express.json());
 
@@ -32,6 +34,7 @@ app.use("/api/transactions", transactionRoutes);
 app.use("/api/customer", customerRoutes);
 app.use("/api/typesaving", typeSavingRoutes);
 app.use("/api/report", reportRoutes);
+app.use("/api/branch", branchRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
