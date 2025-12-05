9933b5322160cc60f20ff3a65b5b89e2ee6e0f47
Phuc Hau <phuchau.fit@gmail.com>
2025-12-05 14:19:23 +0700

    fix: add patch to ignore cors

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ setupSwagger(app);
 app.use(
   cors({
     origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true, // nếu cần gửi cookie
   })
 );
